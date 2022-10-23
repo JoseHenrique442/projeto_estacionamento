5e2d317ad9c5081fd1b5dc8245dceeecf98f7d16
José H. Alves de Azevedo <111004717+JoseHenrique442@users.noreply.github.com>
2022-10-22 22:56:42 -0300

    Ativade Extra - Cadastro de Motorista

--- src/connection/script.sql
+++ src/connection/script.sql
@@ -13,9 +13,18 @@ USE estacionamento;
 
 CREATE TABLE vaga {
     idVaga int NOT NULL AUTO_INCREMENT,
-    numero int NOT NUL,
+    numero int NOT NULL,
     rua varchar(100) NOT NULL,
     obliqua boolean NOT NULL,
     PRIMARY KEY (idVaga)};
 
-/*comentario*/
+CREATE TABLE motorista {
+    idMotorista int NOT NULL AUTO_INCREMENT,
+    nome varchar(100),
+    genero varchar(30),
+    rg varchar(14),
+    cpf varchar(14),
+    celular varchar(14),
+    email varchar(100),
+    senha varchar(50),
+    PRIMARY KEY (idMotorista)};
---
+++ src/model/bean/Motorista.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.bean;
+
+/**
+ *
+ * @author José Henrique
+ */
+public class Motorista {
+    private int idMotorista;
+    private String nome;
+    private String genero;
+    private String rg;
+    private String cpf;
+    private String celular;
+    private String email;
+    private String senha;
+
+    public int getIdMotorista() {
+        return idMotorista;
+    }
+
+    public void setIdMotorista(int idMotorista) {
+        this.idMotorista = idMotorista;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
+    
+
+}
