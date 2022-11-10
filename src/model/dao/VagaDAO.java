3e507d5c30c755b00d6fab9e1351960033778d82
04031966040 <04031966040@LAB205-24.fw.iffar.edu.br>
2022-11-10 20:55:40 -0200

    Semana 7 - Editar motora

--- src/model/dao/VagaDAO.java
+++ src/model/dao/VagaDAO.java
@@ -78,4 +78,47 @@ public class VagaDAO {
             ConnectionFactory.closeConnection(con, stmt);
         }    
     }
+    public Vaga read(int idVaga) {
+        Connection con = ConnectionFactory.getConnection();
+        PreparedStatement stmt = null;
+        ResultSet rs =  null;
+        Vaga v = new Vaga();
+        try {
+            stmt = con.prepareStatement("SELECT * FROM vaga WHERE idVaga =? LIMIT 1;");
+            stmt.setInt(1, idVaga);
+            rs = stmt.executeQuery();
+            if (rs != null && rs.next()) {
+                v.setIdVaga(rs.getInt("idVaga"));
+                v.setNumero(rs.getInt("numero"));
+                v.setRua(rs.getString("rua"));
+                v.setObliqua(rs.getBoolean("obliqua"));
+            }
+        } 
+        catch (SQLException e) {
+            throw new RuntimeException("Erro ao buscar dados: " + e);
+        } 
+        finally {
+            ConnectionFactory.closeConnection(con, stmt, rs);
+        }
+        return v;
+    }    
+    public void update(Vaga v) {
+        Connection con = ConnectionFactory.getConnection();
+        PreparedStatement stmt = null;
+        try {
+            stmt = con.prepareStatement("UPDATE vaga SET numero=?, rua=?, obliqua=? WHERE idVaga=?");
+            stmt.setInt(1, v.getNumero());
+            stmt.setString(2,v.getRua());
+            stmt.setBoolean(3,v.isObliqua());
+            stmt.setInt(4, v.getIdVaga());
+            stmt.executeUpdate();
+            JOptionPane.showMessageDialog(null, "Vaga atualizada com sucesso!");
+        }
+        catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Erro ao atualizar:" + e);
+        }
+        finally {
+            ConnectionFactory.closeConnection(con, stmt);
+        }
+    }
 }
